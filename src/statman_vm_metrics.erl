52d2c0faca3273473befc878d036acb3caa66a42
karlma <karlcpp@gmail.com>
2017-11-25 11:41:12 +0800

    增加了ets和mnesia表监控

--- src/statman_vm_metrics.erl
+++ src/statman_vm_metrics.erl
@@ -10,8 +10,8 @@ get_counters() ->
 
 
 get_gauges() ->
-    Memory = lists:map(fun ({K, V}) ->
-                               {{vm_memory, K}, V}
+    Memory = lists:map(fun({K, V}) ->
+        {{vm_memory, K}, V}
                        end, erlang:memory()),
 
     RunQueue = {{vm, run_queue}, erlang:statistics(run_queue)},
@@ -22,40 +22,43 @@ get_gauges() ->
 
 message_stats() ->
     ProcessInfo = lists:flatmap(
-                    fun (Pid) ->
-                            case process_info(Pid, message_queue_len) of
-                                undefined ->
-                                    [];
-                                {message_queue_len, 0} ->
-                                    [];
-                                {message_queue_len, Count} ->
-                                    [{Count, Pid}]
-                            end
-                    end, processes()),
+        fun(Pid) ->
+            case process_info(Pid, message_queue_len) of
+                undefined ->
+                    [];
+                {message_queue_len, 0} ->
+                    [];
+                {message_queue_len, Count} ->
+                    [{Count, Pid}]
+            end
+        end, processes()),
     TotalQueue = lists:sum(element(1, lists:unzip(ProcessInfo))),
 
     [{{vm, processes_with_queues}, length(ProcessInfo)},
-     {{vm, messages_in_queue}, TotalQueue}].
+        {{vm, messages_in_queue}, TotalQueue}].
 
 ets_stats() ->
-    {Ets, TotalSize} = lists:mapfoldl(fun (T, Acc) ->
-                                    case ets:info(T, size) of
-                                        N when is_integer(N) ->
-                                            {{T,size}, Acc + N};
-                                        undefined ->
-                                            {{T, 0}, Acc}
-                                    end
-                            end, 0, ets:all()),
-    Top10 = lists:sublist(lists:keysort(2, Ets), 1, 10),
-    [{{vm_ets, total_size}, TotalSize} | lists:map(fun ({ET, Size}) -> {{vm_ets, ET}, Size} end, Top10)].
+    EtsList = lists:filtermap(fun(T) when is_atom(T) ->
+        {true, case ets:info(T, size) of
+                   N when is_integer(N) -> {T, N};
+                   undefined -> {T, 0}
+               end};
+        (_) -> false
+                              end,
+        ets:all()),
+    TotalSize = lists:sum(lists:map(fun({_, Size}) -> Size end, EtsList)),
+
+    Top10 = lists:sublist(lists:reverse(lists:keysort(2, EtsList)), 1, 10),
+    io:format("ets table top10:~p~n", [Top10]),
+    [{{vm_ets, total_size}, TotalSize} | lists:map(fun({ET, Size}) -> {{vm_ets, ET}, Size} end, Top10)].
 
 mnesia_table_size() ->
     SessionSize = mnesia:table_info(session, size),
-    %%LastActivitySize = mnesia:table_info(last_activity, size),
-    %%VcardSize = mnesia:table_info(vcard, size),
+    LastActivitySize = mnesia:table_info(last_activity, size),
+    VcardSize = mnesia:table_info(vcard, size),
     [{{mnesia, session_size}, SessionSize}
-    %%,{{mnesia, last_activity_size}, LastActivitySize}
-    %%,{{mnesia, vcard_size}, VcardSize}
+        , {{mnesia, last_activity_size}, LastActivitySize}
+        , {{mnesia, vcard_size}, VcardSize}
     ].
 
 io(undefined) ->
